f9f8a0797a61e40167910384c79e821b8eca3b88
unknown <shuklinavaleria@yandex.ru>
2022-02-15 19:17:31 +0300

    Bug fix

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         int startBalance = 300;
-        int replenishment = 600;
+        int replenishment = 1600;
         int balance = startBalance + replenishment;
 
         int bonus;
-        if (replenishment>1000) {
-            bonus = 1*replenishment;}
-        else {
-            bonus = 0;}
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
 
         int totalBalance = balance + bonus;
 
-                    System.out.println (("Ваш баланс ") + (totalBalance));
+        System.out.println(("Ваш баланс ") + (totalBalance));
 
     }
 
